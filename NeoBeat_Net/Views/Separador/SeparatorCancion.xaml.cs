5677050ed203f8cf6a8e6374637dc1273afd540d
olipite <oliver.tazueco.baraza@iestubalcain.net>
2025-12-24 22:30:08 +0100

    GUI, optimizacion, Views separators

---
+++ NeoBeat_Net/Views/Separador/SeparatorCancion.xaml.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeoBeat_Net.Views.Separador;
+
+public partial class SeparatorCancion : ContentView
+{
+    public SeparatorCancion()
+    {
+        InitializeComponent();
+    }
+}
