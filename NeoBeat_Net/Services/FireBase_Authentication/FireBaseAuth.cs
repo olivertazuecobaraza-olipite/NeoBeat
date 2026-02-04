84b14f2ef092b5510d404e81d8ef98655fd9cc7d
Oliver Tazueco <oliver.tazueco.baraza@iestubalcain.net>
2026-02-04 17:39:42 +0100

    remove firebase credentials

--- NeoBeat_Net/Services/FireBase_Authentication/FireBaseAuth.cs
+++ NeoBeat_Net/Services/FireBase_Authentication/FireBaseAuth.cs
@@ -5,18 +5,7 @@ namespace NeoBeat_Net.Services.FireBase_Authentication;
 
 public class FireBaseAuth : IFireBaseAuth
 {
-    /*
-     
-     const firebaseConfig = {
-           apiKey: "AIzaSyDTlcb264JfxCFVweNkSJZCoFSbxHPey4I",
-           authDomain: "neobeat-3d798.firebaseapp.com",
-           projectId: "neobeat-3d798",
-           storageBucket: "neobeat-3d798.firebasestorage.app",
-           messagingSenderId: "1005304689766",
-           appId: "1:1005304689766:web:17e3bbcc7d1cf69e5b69a7"
-       };
-     
-     */
+    
     #region Instance
     
     public FirebaseAuthClient Instance { get; } = new FirebaseAuthClient(new FirebaseAuthConfig
